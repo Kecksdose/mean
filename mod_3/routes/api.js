99f715a0812ad8aa7e5dff882b77441ed25f2a6e
Timon <timon.schmelzer@tu-dortmund.de>
2016-11-09 23:41:44 +0100

    1:43:00

--- mod_3/routes/api.js
+++ mod_3/routes/api.js
@@ -14,4 +14,21 @@ router.route('/posts')
     res.send({message: 'TODO create a new post'});
   });
 
+router.route('/posts/:id')
+  //return a particular post
+  .get(function(req, res){
+    res.send({message: 'TODO return post with ID ' + req.params.id})
+  })
+
+  //update existing post
+  .put(function(req, res){
+    res.send({message: 'TODO modify post with ID ' + req.params.id})
+  })
+
+  //delete existing post
+  .delete(function(req, res){
+    res.send({message: 'TODO delete post with ID ' + req.params.id})
+  })
+
+
 module.exports = router;
